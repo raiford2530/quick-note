8f0394ab73cfa0b59af287812f38a5d860ef36a3
Rashawn Raiford <raiford87@gmail.com>
2020-11-12 19:46:30 -0500

    Add route for index.html

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Quick Note listening on http://localhost:${PORT}`)
 })
+
+const publicRoot = path.join(__dirname, "public");
+
+//ROUTES
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicRoot, 'index.html'));
+})
+
