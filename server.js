7ecc67236f5ea1110fe42354d40204b092cd2d51
Rashawn Raiford <raiford87@gmail.com>
2020-11-13 21:09:29 -0500

    Added comments

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require('path');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
+
+//Create instance of server
 const app = express();
 
 app.use(express.static('public'));
+//Middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
@@ -15,14 +18,16 @@ app.listen(PORT, () => {
 
 const publicRoot = path.join(__dirname, "public");
 
-//VIEW ROUTES
+//GET /notes 
+//Return note.html file
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(publicRoot, 'notes.html'));
 })
 
 const dbPath = path.join(__dirname, "db/db.json");
 
-//API ROUTES
+///GET /api/notes
+//Return all notes from db.json file
 app.get('/api/notes', (req, res) => {
     fs.readFile(dbPath, (err, data) => {
         res.json(JSON.parse(data));
@@ -30,16 +35,22 @@ app.get('/api/notes', (req, res) => {
 })
 
 let noteID = 1;
+
+//POST /api/notes
+//Retreive data from form and assign it an id and then save it to db.json
 app.post('/api/notes', (req, res) => {
     let note = req.body;
     note.id = noteID++;
 
+    //Get json from db.json to retrieve the array of notes 
+    //and then add the note from the request to the array
     fs.readFile(dbPath, (err, data) => {
         if(err) throw err;
 
         const notes = JSON.parse(data);
         notes.push(note);
 
+        //Write the modified array as a json object to db.json
         fs.writeFile(dbPath, JSON.stringify(notes), "utf8", (err) => {
             if(err) throw err;
 
@@ -48,19 +59,25 @@ app.post('/api/notes', (req, res) => {
     })   
 })  
 
+//DELETE /api/notes/:id
+//Delete note with the specified id from the request
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = parseInt(req.params.id);
 
+    //Get json from db.json to retrieve the array of notes 
     fs.readFile(dbPath, (err, data) => {
         if(err) throw err;
 
         let notes = JSON.parse(data);
         
+        //Find the index of the note with the id from the request
         const deleteIndex = notes.findIndex(note => note.id === noteId)
 
         if(deleteIndex > -1){
+            //Delete the note from the array
             notes.splice(deleteIndex, 1);
 
+            //Write the modified array as a json object to db.json
             fs.writeFile(dbPath, JSON.stringify(notes), "utf8", (err) => {
                 if(err) throw err;
     
@@ -72,6 +89,8 @@ app.delete('/api/notes/:id', (req, res) => {
     })   
 })
 
+//GET *
+//Send the index.html file for all routes that doesn't match our created routes
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicRoot, 'index.html'));
 })
